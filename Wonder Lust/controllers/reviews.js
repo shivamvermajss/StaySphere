377d1fc666857ebb165a64692444f4c2813dfbb0
Shivam Verma <inocentshivam2002@gmail.com>
2026-01-12 16:59:50 +0530

    Fix review submission breaking due to listing category validation

--- Wonder Lust/controllers/reviews.js
+++ Wonder Lust/controllers/reviews.js
@@ -1,29 +1,31 @@
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 
-
 module.exports.createReview = async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
+    const listing = await Listing.findById(req.params.id);
+
+    const newReview = new Review(req.body.review);
     newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
+
     await newReview.save();
-    await listing.save();
+
+    await listing.save({ validateBeforeSave: false });
 
     req.flash("success", "Successfully added a new review!");
     res.redirect(`/listings/${listing._id}`);
 };
 
 module.exports.destroyReview = async (req, res) => {
-        let { id, reviewId } = req.params;
+    const { id, reviewId } = req.params;
 
-        await Listing.findByIdAndUpdate(id, {
-            $pull: { reviews: reviewId }
-        });
+    await Listing.findByIdAndUpdate(id, {
+        $pull: { reviews: reviewId }
+    });
 
-        await Review.findByIdAndDelete(reviewId);
+    await Review.findByIdAndDelete(reviewId);
 
-        req.flash("success", "Successfully deleted review!");
-        res.redirect(`/listings/${id}`);
-    }
+    req.flash("success", "Successfully deleted review!");
+    res.redirect(`/listings/${id}`);
+};
